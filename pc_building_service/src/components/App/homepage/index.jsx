4a88722756483a89b74a4bca0db444c98bf45e7d
jonah-angelo <jonah.angelo@gmail.com>
2024-03-20 21:55:28 -0700

    scaffolded application

---
+++ pc_building_service/src/components/App/homepage/index.jsx
@@ -0,0 +1,7 @@
+export default function Homepage() {
+    return (
+        <main>
+            <h1>Homepage Component</h1>
+        </main>
+    )
+}
---
+++ pc_building_service/src/components/App/index.jsx
@@ -0,0 +1,20 @@
+import './styles.css'
+
+function App() {
+  return (
+    <>
+        <nav>
+            <a href="/">Home</a>
+            <a href="/about">About</a>
+            <a href="/contact">Contact</a>
+            <a href="/user">User Profile</a>
+        </nav>
+
+        <main>
+            <h1>App Component</h1>
+        </main>
+    </>
+  )
+}
+
+export default App
